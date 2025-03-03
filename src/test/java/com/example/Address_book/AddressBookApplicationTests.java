d2b7cfa8dd760f7f7f6294a14f77fbb0009703ce
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-03-03 12:07:53 +0530

    Saurabh Add . Initial Address_book

---
+++ src/test/java/com/example/Address_book/AddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Address_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
