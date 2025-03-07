dc5dd4be5868bf22926f3e1dd802f69c1f42cab3
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-03-07 11:39:30 +0530

    Saurabh Add . Copied UC5 content into main

--- src/main/java/com/example/Address_book/controller/ContactController.java
+++ src/main/java/com/example/Address_book/controller/ContactController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200") // Allow Angular frontend
 @RestController
 @RequestMapping("/api/contacts")
 public class ContactController {
